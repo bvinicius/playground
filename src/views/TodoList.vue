a8550c82029806772659ab22419ff9661ca54148
vbazanella <vinicius.bazanella1@gmail.com>
2020-07-28 20:53:05 -0300

    Adds transitions to the components

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,39 +1,41 @@
 <template>
   <v-main style="height:100%">
+    <v-fade-transition>
+      <v-row justify="center">
+        <v-col cols="6">
+            <v-text-field
+              autocomplete="off"
+              placeholder="Ex.: Walk the dog..."
+              autofocus
+              style="height:60px"
+              v-on:keydown.enter="addTask()"
+              v-model="newTask"
+              append-icon="filter_list"
+            >
+            </v-text-field>
+        </v-col>
+      </v-row>
+    </v-fade-transition>
     <v-row justify="center">
       <v-col cols="6">
-          <v-text-field
-            autocomplete="off"
-            placeholder="Ex.: Walk the dog"
-            autofocus
-            style="height:60px"
-            v-on:keydown.enter="addTask()"
-            v-model="displayTasks"
-          >
-          </v-text-field>
-      </v-col>
-    </v-row>
-
-    <v-row justify="center">
-      <v-col cols="6">
-        <v-list>
-          <v-list-item
-            link
-            v-for="(item, i) in tasks" :key="i"
-            @click="item.done = !item.done"
-          >
-            <v-list-item-content>
-              {{ item.name }}
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-checkbox v-model="item.done">
-              </v-checkbox>
-            </v-list-item-action>
-          </v-list-item>
+        <v-list class="py-0">
+          <v-slide-y-transition group>
+            <v-list-item
+              link
+              v-for="(item, i) in tasks" :key="i"
+              @click="item.done = !item.done"
+            >
+              <v-list-item-action>
+                <v-checkbox v-model="item.done" @click.prevent></v-checkbox>
+              </v-list-item-action>
+              <v-list-item-content>
+                {{ item.name }}
+              </v-list-item-content>
+            </v-list-item>
+          </v-slide-y-transition>
         </v-list>
       </v-col>
     </v-row>
-
   </v-main>
 </template>
   
@@ -41,7 +43,7 @@
 
 export default {
   name: 'TodoList',
-  filters: [
+  filteropts: [
     {
       name: 'Done',
       value: task => task.done
@@ -59,11 +61,7 @@ export default {
     newTask: ''
   }),
 
-  computed: {
-    displayTasks() {
-      return this.tasks.filter(e => this.filter(e))
-    }
-  },
+  computed: {},
 
   methods: {
     addTask: function() {
@@ -72,7 +70,6 @@ export default {
           name: this.newTask,
           done: false,
         })
-
         this.newTask = ''
       }
     }
