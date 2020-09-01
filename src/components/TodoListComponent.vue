52410361d4689cacb0cecf58a24968dd8b106291
vbazanella <vinicius.bazanella1@gmail.com>
2020-09-01 10:08:40 -0300

    Adds transition to todolist card

--- src/components/TodoListComponent.vue
+++ src/components/TodoListComponent.vue
@@ -1,56 +1,58 @@
 <template>
   <v-main style="height:100%">
     <v-row justify="center">
-      <v-card width="600" class="px-4">
-        <v-fade-transition>
+      <v-slide-y-transition>
+        <v-card width="600" class="px-4">
+          <v-fade-transition>
+            <v-row justify="center">
+              <v-col cols="12">
+                  <v-text-field
+                    autocomplete="off"
+                    placeholder="Ex.: Walk the dog..."
+                    autofocus
+                    style="height:60px"
+                    v-on:keydown.enter="addTask()"
+                    v-model="newTask"
+                    append-icon="filter_list"
+                  >
+                  </v-text-field>
+              </v-col>
+            </v-row>
+          </v-fade-transition>
           <v-row justify="center">
             <v-col cols="12">
-                <v-text-field
-                  autocomplete="off"
-                  placeholder="Ex.: Walk the dog..."
-                  autofocus
-                  style="height:60px"
-                  v-on:keydown.enter="addTask()"
-                  v-model="newTask"
-                  append-icon="filter_list"
-                >
-                </v-text-field>
+              <v-list class="py-0">
+                <v-scroll-y-transition group>
+                  <v-list-item
+                    link
+                    v-for="(item, i) in showtasks" :key="i"
+                    @click="item.done = !item.done"
+                  >
+                    <v-list-item-action>
+                      <v-checkbox v-model="item.done" @click.prevent></v-checkbox>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                      {{ item.name }}
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-scroll-y-transition>
+              </v-list>
             </v-col>
           </v-row>
-        </v-fade-transition>
-        <v-row justify="center">
-          <v-col cols="12">
-            <v-list class="py-0">
-              <v-scroll-y-transition group>
-                <v-list-item
-                  link
-                  v-for="(item, i) in showtasks" :key="i"
-                  @click="item.done = !item.done"
-                >
-                  <v-list-item-action>
-                    <v-checkbox v-model="item.done" @click.prevent></v-checkbox>
-                  </v-list-item-action>
-                  <v-list-item-content>
-                    {{ item.name }}
-                  </v-list-item-content>
-                </v-list-item>
-              </v-scroll-y-transition>
-            </v-list>
-          </v-col>
-        </v-row>
 
-        <v-card-actions>
-          <v-row justify="center">
-            <v-btn-toggle
-              tile
-            >
-              <v-btn @click="filter = ''">All</v-btn>
-              <v-btn @click="filter = 'done'">To do</v-btn>
-              <v-btn @click="filter = 'todo'">Done</v-btn>
-            </v-btn-toggle>
-          </v-row>
-        </v-card-actions>
-      </v-card>
+          <v-card-actions>
+            <v-row justify="center">
+              <v-btn-toggle
+                tile
+              >
+                <v-btn @click="filter = ''">All</v-btn>
+                <v-btn @click="filter = 'done'">To do</v-btn>
+                <v-btn @click="filter = 'todo'">Done</v-btn>
+              </v-btn-toggle>
+            </v-row>
+          </v-card-actions>
+        </v-card>
+      </v-slide-y-transition>
     </v-row>
   </v-main>    
 </template>
